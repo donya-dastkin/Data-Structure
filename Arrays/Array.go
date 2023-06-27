5160f49a47f1f48397a99bee593f6e7a89e96a23
donya <donixcdcd@gmail.com>
2023-06-27 12:54:12 +0430

    added length

--- Arrays/Array.go
+++ Arrays/Array.go
@@ -4,5 +4,7 @@ import "fmt"
 
 func main() {
 	arr := [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	var length int = len(arr)
+	fmt.Println("Array Length :", length)
 	fmt.Println("Array Element :", arr)
 }
